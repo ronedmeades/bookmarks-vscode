c34310f96fc077bf9c5d332feddd68a61974c267
ronedmeades <ron.edmeades@gmail.com>
2025-08-04 02:56:25 -0700

    Have basic functionality working, working on adding gutter icons

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,250 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Bookmarks extension is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bookmarks" is now active!');
+    const bookmarkProvider = new BookmarkProvider(context);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('bookmarks.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Bookmarks!');
+	const bookmarkDecoration = vscode.window.createTextEditorDecorationType({
+		before: {
+			contentText: '$(bookmark)',
+			margin: '0 0.2em 0 0',
+			color: new vscode.ThemeColor('bookmarks.gutterIconColor')
+		},
+		overviewRulerColor: 'blue', 
+		overviewRulerLane: vscode.OverviewRulerLane.Left
 	});
 
-	context.subscriptions.push(disposable);
+    // Register the tree data provider
+    vscode.window.registerTreeDataProvider('bookmarksList', bookmarkProvider);
+    
+    // Load existing bookmarks
+    bookmarkProvider.loadBookmarks();
+
+    // Add bookmark command
+	const addBookmarkCommand = vscode.commands.registerCommand('bookmarks.add', async (context?: {lineNumber: number, uri: vscode.Uri}) => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showErrorMessage('No active editor');
+			return;
+		}
+		
+		const lineNumber = context?.lineNumber ? context.lineNumber - 1 : editor.selection.active.line;
+		
+		// Check if bookmark already exists
+		const fileKey = editor.document.uri.toString();
+		const fileBookmarks = bookmarkProvider.getBookmarks(fileKey) || [];
+		const existingBookmark = fileBookmarks.find(b => b.line === lineNumber);
+		
+		if (existingBookmark) {
+			vscode.window.showInformationMessage('Bookmark already exists at this line');
+			return;
+		}
+		
+		bookmarkProvider.addBookmark(editor.document.uri, lineNumber);
+		vscode.window.showInformationMessage('Bookmark added');
+	});
+
+	// Remove bookmark command  
+	const removeBookmarkCommand = vscode.commands.registerCommand('bookmarks.remove', async (context?: {lineNumber: number, uri: vscode.Uri}) => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showErrorMessage('No active editor');
+			return;
+		}
+		
+		const lineNumber = context?.lineNumber ? context.lineNumber - 1 : editor.selection.active.line;
+		
+		const wasRemoved = bookmarkProvider.removeBookmark(editor.document.uri, lineNumber);
+		
+		if (wasRemoved) {
+			vscode.window.showInformationMessage('Bookmark removed');
+		} else {
+			vscode.window.showInformationMessage('No bookmark found at this line');
+		}
+	});
+
+    context.subscriptions.push(addBookmarkCommand, removeBookmarkCommand);
 }
 
-// This method is called when your extension is deactivated
+interface Bookmark {
+    line: number;
+    label?: string;
+}
+
+class BookmarkProvider implements vscode.TreeDataProvider<BookmarkItem> {
+    private _onDidChangeTreeData: vscode.EventEmitter<BookmarkItem | undefined | null | void> = new vscode.EventEmitter<BookmarkItem | undefined | null | void>();
+    readonly onDidChangeTreeData: vscode.Event<BookmarkItem | undefined | null | void> = this._onDidChangeTreeData.event;
+    
+    private bookmarks: Map<string, Bookmark[]> = new Map();
+    private context: vscode.ExtensionContext;
+	private bookmarkDecoration: vscode.TextEditorDecorationType; // Add this
+
+    constructor(context: vscode.ExtensionContext) {
+        this.context = context;
+        
+        // Create the decoration type
+        this.bookmarkDecoration = vscode.window.createTextEditorDecorationType({
+            before: {
+                contentText: 'BM', // Use visible text instead of $(bookmark)
+				margin: '0 0.2em 0 0',
+				color: 'red',      // Make it bright red so we can see it
+				fontWeight: 'bold'
+            },
+            overviewRulerColor: 'blue', 
+            overviewRulerLane: vscode.OverviewRulerLane.Left
+        });
+    }
+
+    refresh(): void {
+        this._onDidChangeTreeData.fire();
+    }
+
+    loadBookmarks(): void {
+        const stored = this.context.workspaceState.get<{[key: string]: Bookmark[]}>('bookmarks', {});
+        this.bookmarks = new Map(Object.entries(stored));
+        this.refresh();
+		this.updateDecorations();
+		// To forcefully clear the bookmarks files!
+		// this.bookmarks = new Map();
+		// this.context.workspaceState.update('bookmarks', {});
+		// this.refresh();
+		// console.log('Bookmarks forcibly cleared');
+		// this.refresh();
+    }
+
+    private async saveBookmarks(): Promise<void> {
+        const bookmarksObject = Object.fromEntries(this.bookmarks);
+        await this.context.workspaceState.update('bookmarks', bookmarksObject);
+    }
+
+	getBookmarks(fileKey: string): Bookmark[] | undefined {
+    	return this.bookmarks.get(fileKey);
+	}
+
+	addBookmark(fileUri: vscode.Uri, line: number, label?: string): void {
+		const fileKey = fileUri.toString();
+		const fileBookmarks = this.bookmarks.get(fileKey) || [];
+		
+		const newBookmark: Bookmark = { line, label };
+		fileBookmarks.push(newBookmark);
+		fileBookmarks.sort((a, b) => a.line - b.line);
+		this.bookmarks.set(fileKey, fileBookmarks);
+		this.saveBookmarks();
+		this.refresh();
+		this.updateDecorations();
+	}
+
+	removeBookmark(fileUri: vscode.Uri, line: number): boolean {
+		const fileKey = fileUri.toString();
+		const fileBookmarks = this.bookmarks.get(fileKey) || [];
+		
+		const existingIndex = fileBookmarks.findIndex(b => b.line === line);
+		if (existingIndex >= 0) {
+			fileBookmarks.splice(existingIndex, 1);
+			if (fileBookmarks.length === 0) {
+				this.bookmarks.delete(fileKey);
+			} else {
+				this.bookmarks.set(fileKey, fileBookmarks);
+			}
+			this.saveBookmarks();
+			this.refresh();
+			this.updateDecorations();
+			return true;
+		}
+		return false;
+	}
+
+	updateDecorations(): void {
+		// Update decorations for all visible editors
+		vscode.window.visibleTextEditors.forEach(editor => {
+			const fileKey = editor.document.uri.toString();
+			const fileBookmarks = this.bookmarks.get(fileKey) || [];
+			
+			// Create decoration ranges for this file's bookmarks
+			const decorationRanges = fileBookmarks.map(bookmark => 
+				new vscode.Range(bookmark.line, 0, bookmark.line, 0)
+			);
+			
+			// Apply decorations to this editor
+			editor.setDecorations(this.bookmarkDecoration, decorationRanges);
+		});
+	}
+
+    getTreeItem(element: BookmarkItem): vscode.TreeItem {
+        if (element.type === 'file') {
+            const item = new vscode.TreeItem(element.label, vscode.TreeItemCollapsibleState.Expanded);
+            item.iconPath = vscode.ThemeIcon.File;
+            return item;
+        } else {
+            const item = new vscode.TreeItem(element.label, vscode.TreeItemCollapsibleState.None);
+            item.iconPath = new vscode.ThemeIcon('bookmark');
+            
+            // Command to navigate to bookmark
+            item.command = {
+                command: 'vscode.open',
+                title: 'Go to Bookmark',
+                arguments: [
+                    element.fileUri,
+                    { selection: new vscode.Range(element.line!, 0, element.line!, 0) }
+                ]
+            };
+            
+            item.contextValue = 'bookmark';
+            return item;
+        }
+    }
+
+    async getChildren(element?: BookmarkItem): Promise<BookmarkItem[]> {
+        if (!element) {
+            // Root level - return file groups
+            const fileItems: BookmarkItem[] = [];
+            
+            for (const [fileUriString, bookmarks] of this.bookmarks) {
+                if (bookmarks.length > 0) {
+                    const fileUri = vscode.Uri.parse(fileUriString);
+                    const fileName = fileUri.path.split('/').pop() || fileUriString;
+                    
+                    fileItems.push({
+                        type: 'file',
+                        label: fileName,
+                        fileUri: fileUri,
+                        bookmarks: bookmarks
+                    });
+                }
+            }
+            
+            return fileItems;
+        } else if (element.type === 'file') {
+            // File level - return bookmarks for this file
+            const bookmarkItems: BookmarkItem[] = [];
+            
+            for (const bookmark of element.bookmarks || []) {
+                const label = bookmark.label 
+                    ? `Line ${bookmark.line + 1}: ${bookmark.label}`
+                    : `Line ${bookmark.line + 1}`;
+                
+                bookmarkItems.push({
+                    type: 'bookmark',
+                    label: label,
+                    fileUri: element.fileUri,
+                    line: bookmark.line
+                });
+            }
+            
+            return bookmarkItems;
+        }
+        
+        return [];
+    }
+}
+
+interface BookmarkItem {
+    type: 'file' | 'bookmark';
+    label: string;
+    fileUri?: vscode.Uri;
+    line?: number;
+    bookmarks?: Bookmark[];
+}
+
 export function deactivate() {}
